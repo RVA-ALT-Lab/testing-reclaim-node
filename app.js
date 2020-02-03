0dff5b19bcdb543281e80f672b98f3a658ea7ab5
Jeff Everhart <Jeffeverhart383@gmail.com>
2020-02-03 14:57:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! From short URL'))
+app.get('/testing-reclaim-node', (req, res) => res.send('Hello World! From long url'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
